508019027526c3aafc49f2b35f46f767ff57bd6a
lan <1461489135@qq.com>
2020-02-10 20:16:33 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+      <h2>后台首页</h2>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h2>管理员登录</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+      name:'home'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
