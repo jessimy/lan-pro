a545c24a1b21f004e92567770cf3064d9ffdb7b2
lan <1461489135@qq.com>
2020-02-10 21:45:32 +0800

    组件配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'amfe-flexible/index.min.js'
+import '@/assets/css/global.less'
 Vue.config.productionTip = false
-
+Vue.use (Vant)
 new Vue({
   router,
   store,
